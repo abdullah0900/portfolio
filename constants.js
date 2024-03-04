47325d0964aebaccab7b2938996490e0b82a8cd8
abdullah0900 <ranaabdullah510@gmail.com>
2024-03-05 00:17:09 +0500

    uploaded

---
+++ constants.js
@@ -0,0 +1,206 @@
+export const METADATA = {
+  author: "Abdullah Younas",
+  title: "Portfolio | Abdullah Younas",
+  description:
+    "Abdullah Younas is a Web Developer from Pakistan, interested in crafting beautiful and functional web applications.",
+  siteUrl: "https://www.shubhporwal.me/",
+  twitterHandle: "@ranaabdullah510",
+  keywords: [
+    "Abdullah Younas",
+    "Web Developer",
+    "React Native Developer",
+    "Software Developer",
+    "Software Engineer",
+    "Portfolio",
+    "Devfolio",
+    "Folio",
+  ].join(", "),
+  image:
+    "https://res.cloudinary.com/dywdhyojt/image/upload/v1690572126/preview.png",
+  language: "English",
+};
+
+export const MENULINKS = [
+  {
+    name: "Home",
+    ref: "home",
+  },
+  {
+    name: "Skills",
+    ref: "skills",
+  },
+  {
+    name: "Projects",
+    ref: "projects",
+  },
+  {
+    name: "Work",
+    ref: "work",
+  },
+  {
+    name: "Contact",
+    ref: "contact",
+  },
+];
+
+export const TYPED_STRINGS = [
+  "A Software Developer",
+  "I build things for the web",
+  "I create aesthetic and modern apps",
+];
+
+export const SOCIAL_LINKS = [
+  {
+    name: "mail",
+    url: "mailto: abdullah.younas540@gmail.com",
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/in/abdullahrajput11/",
+  },
+  {
+    name: "github",
+    url: "https://github.com/abdullah0900",
+  },
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/mail_4_abdullah/",
+  },
+  {
+    name: "twitter",
+    url: "https://twitter.com/ranaabdullah510",
+  },
+];
+
+export const SKILLS = {
+  languagesAndTools: [
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "cpp",
+    "java",
+    "python",
+    "nodejs",
+    "webpack",
+    "vite",
+    "stripe",
+    "figma",
+  ],
+  librariesAndFrameworks: [
+    "react",
+    "redux",
+    "nextjs",
+    "tailwindcss",
+    "styledcomponents",
+    "chakra-ui",
+  ],
+  databases: ["mysql", "mongodb"],
+  other: ["git"],
+};
+
+export const PROJECTS = [
+  // {
+  //   name: "Shotime",
+  //   image: "",
+  //   blurImage: "",
+  //   description:
+  //     "Developed with complete E-commerce functionality and User Authentication 🛍️",
+  //   gradient: ["#FFCF1B", "#FF881B"],
+  //   url: "https://shubh73-shotime.vercel.app/",
+  //   tech: ["react", "tailwindcss", "redux", "mongodb", "stripe"],
+  // },
+  {
+    name: "EditOn",
+    image: "/projects/editon.webp",
+    blurImage: "/projects/blur/airbnb-blur.webp",
+    description: "Editon project using React + JSX + Tailwind CSS 🛏️",
+    gradient: ["#F14658", "#DC2537"],
+    url: "https://github.com/abdullah0900/EditOn",
+    tech: ["react", "nextjs", "tailwindcss", "mapbox"],
+  },
+  {
+    name: "Bhaichara.pk",
+    image: "/projects/medium.webp",
+    blurImage: "/projects/blur/medium-blur.webp",
+    description: "Medium UI clone using NextJS + Tailwind CSS ✍🏻",
+    gradient: ["#FFA62E", "#EA4D2C"],
+    url: "https://shubh73-medium.vercel.app/",
+    tech: ["typescript", "react", "nextjs", "tailwindcss", "sanity.io"],
+  },
+  {
+    name: "FutureCareerConsultancy",
+    image: "/projects/fcc.webp",
+    blurImage: "/projects/blur/airbnb-blur.webp",
+    description:
+      "Conversational Voice Controlled React News Application using Alan AI 🎙",
+    gradient: ["#000066", "#6699FF"],
+    url: "https://github.com/abdullah0900/futureCareerConsultancy",
+    tech: ["react", "chakra-ui", "alan"],
+  },
+  {
+    name: "Pokemon Monster",
+    image: "/projects/pokemon.webp",
+    blurImage: "/projects/blur/tesla-blur.webp",
+    description: "A Pokemon Monster Java App 🏎️",
+    gradient: ["#142D46", "#2E4964"],
+    url: "https://github.com/abdullah0900/Pokermon-Monster-Game",
+    tech: ["java"],
+  },
+  {
+    name: "Data Analytics",
+    image: "/projects/tesla.webp",
+    blurImage: "/projects/blur/tesla-blur.webp",
+    description: "A Tesla React Native App 🏎️",
+    gradient: ["#142D46", "#2E4964"],
+    url: "https://github.com/shubh73/tesla",
+    tech: ["python"],
+  },
+];
+
+// export const WORK = [
+//   {
+//     id: 1,
+//     company: "Dukaan",
+//     title: "Frontend Developer",
+//     location: "Bangalore, Karnataka",
+//     range: "December - Current",
+//     responsibilities: [
+//       "Led creation of a captivating cross-platform e-commerce solution.",
+//       "Enhanced UX with gamification and personalized push notifications ensuring an ever-improving shopping journey.",
+//       "The app boasts a DAU base of 13k and an extensive MAU count of 170k.",
+//     ],
+//     url: "https://mydukaan.io/",
+//     video: "/work/dukaan.mp4",
+//   },
+//   {
+//     id: 2,
+//     company: "Aviate",
+//     title: "Frontend Developer Intern",
+//     location: "Goa",
+//     range: "May - October 2022",
+//     responsibilities: [
+//       "Built their flagship product Q-Rate, a voice-based applicant screening platform.",
+//       "Developed pixel-perfect responsive web applications achieving daily traffic of 1000-2000 users.",
+//       "Successfully rolled out an error-logging and bug reporting system that cut user-reported bugs by 30%.",
+//     ],
+//     url: "https://www.aviate.jobs/",
+//     video: "/work/aviate.mp4",
+//   },
+//   {
+//     id: 3,
+//     company: "Spacenos",
+//     title: "Web Developer Intern",
+//     location: "Bangalore, Karnataka",
+//     range: "September - December 2021",
+//     responsibilities: [
+//       "Led the Full Stack revamp on the Admin Portal.",
+//       "Developed app integration with REST APIs, Google Maps, User Auth, Stripe and other libraries.",
+//       "Implemented CRUD features for all the services and providers.",
+//     ],
+//     url: "https://spacenos.com/",
+//     video: "/work/spacenos.mp4",
+//   },
+// ];
+
+export const GTAG = "G-5HCTL2TJ5W";
